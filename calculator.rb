906fc107e3ebe1de673807dc3e7e504517870a06
Katie Seggelke <katie.seggelke@gmail.com>
2019-06-20 20:43:40 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,5 @@
-first_number = ""
-second_number = "" 
+first_number = 7
+second_number = 13 
 sum = first_number + second_number
 difference = first_number - second_number
 product = first_number * second_number
