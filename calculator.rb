6c271c864b659269d9951f618c3484d26dc961bb
Katie Seggelke <katie.seggelke@gmail.com>
2019-06-20 20:38:02 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,5 @@
-first_number = rand(1..1000)
-second_number = rand(1..1000)
+first_number = rand(1..100)
+second_number = rand(1..100)
 sum = first_number + second_number
 difference = first_number - second_number
 product = first_number * second_number
